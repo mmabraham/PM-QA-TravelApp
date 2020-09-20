8482e4bc779d64201091f82545316e5cf06c3af7
Yicheng <yjin@stratfs.com>
2020-09-20 15:43:22 -0400

    fix deploy issue

--- qa-bug-test-app/src/App.js
+++ qa-bug-test-app/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import logo from "./assests/icon/Logo.png";
 import airplane from "./assests/icon/flights.jpg";
 import { Input } from "semantic-ui-react";
-import { data } from "./data/data";
 
 function App() {
   //state
@@ -32,6 +31,53 @@ function App() {
       return parseInt(amountToSave) * parseInt(monthToSave);
     }
   };
+
+  //data
+  const data = [
+    {
+      name: "China",
+      hotelFee: 400,
+      airfareFee: 940,
+      total: 1340,
+      url: "./assests/landmarkImages/UK.jpg",
+    },
+    {
+      name: "Australia",
+      hotelFee: 450,
+      airfareFee: 1300,
+      total: 2000,
+      url: "./assests/landmarkImages/Australia.jpg",
+    },
+    {
+      name: "France",
+      hotelFee: 500,
+      airfareFee: 1250,
+      total: 1750,
+      url: "./assests/landmarkImages/France.jpg",
+    },
+    {
+      name: "Greece",
+      hotelFee: 400,
+      airfareFee: 1000,
+      total: 1400,
+      url: "./assests/landmarkImages/Greece.jpg",
+    },
+    {
+      name: "United Kingdom",
+      hotelFee: 600,
+      airfareFee: 1500,
+      total: 2100,
+      url: "./assests/landmarkImages/china.jpg",
+    },
+    {
+      name: "United Sates of America",
+      hotelFee: 550,
+      airfareFee: 1100,
+      total: 1650,
+      url: "./assests/landmarkImages/united_states.jpg",
+    },
+  ];
+  
 
   return (
     <div className="app">
